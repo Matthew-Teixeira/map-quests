81fd7e4762f00fe6597a48edb763bf21bf296140
Matt Teixeira <matt1teixeira@gmail.com>
2023-01-07 18:42:18 -0500

    Deploy

--- server/server.js
+++ server/server.js
@@ -3,12 +3,26 @@ const express = require("express");
 const app = express();
 const db = require("./db/connection");
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const path = require("path");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static("public"));
+app.use(cors());
 
 app.use(require("./routes"));
 
+// Serve frontend
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    );
+  });
+}
 
 db.once("open", () => {
   app.listen(PORT, () => {
