fadc93bf961d6edd9f6a7c643fada45fd04e0ad1
Matt Teixeira <matt1teixeira@gmail.com>
2023-07-30 18:20:29 -0400

    Increased Z index

--- client/src/components/InnerDropdown.jsx
+++ client/src/components/InnerDropdown.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const InnerDropdown = ({ list }) => {
   return (
-    <ul className="absolute md:right-[-30px] top-8 w-[150px] rounded-md py-2 shadow-xl dark:bg-gray-800 dark:text-[#fdf8ad]">
+    <ul className="absolute md:right-[-30px] top-8 w-[150px] rounded-md py-2 shadow-xl dark:bg-gray-800 dark:text-[#fdf8ad] z-50">
       {
             list.map((link) => (
               <li key={link.name} className="ml-2 text-lg my-2">
